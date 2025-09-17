3d675472444ab57ba1e3f04c444f13997544ebcb
Krzysztof Borowy <6444719+krizzu@users.noreply.github.com>
2025-09-17 11:05:18 +0200

    fix(async-storage): prefer enum to namespace

--- packages/async-storage/src/AsyncStorageError.ts
+++ packages/async-storage/src/AsyncStorageError.ts
@@ -1,29 +1,53 @@
+enum AsyncStorageErrorType {
+  /**
+   * Related to RN Native module itself, ex. not initialized or null at app boot
+   */
+  NativeModuleError = "NativeModuleError",
+
+  /**
+   * Error thrown from Sqlite itself
+   * https://www.sqlite.org/rescode.html
+   */
+  SqliteStorageError = "SqliteStorageError",
+
+  /**
+   * Other errors related to Native Shared Storage
+   * ex. Storage could not be initialized
+   */
+  OtherStorageError = "OtherStorageError",
+
+  /**
+   * Catch-all case, where we can't really tell what went wrong
+   */
+  UnknownError = "UnknownError",
+}
+
 export class AsyncStorageError extends Error {
   private constructor(
     public errorMessage: string,
-    public type: AsyncStorageError.Type
+    public type: AsyncStorageErrorType
   ) {
     super(errorMessage);
     this.name = this.constructor.name;
   }
 
-  static nativeError(e: any): AsyncStorageError {
+  static nativeError(e: unknown): AsyncStorageError {
     const error = getNativeError(e);
     if (!error) {
       return new AsyncStorageError(
-        e?.message ?? `Unknown error ${e}`,
-        AsyncStorageError.Type.UnknownError
+        (e as { message?: string })?.message ?? `Unknown error ${e}`,
+        AsyncStorageErrorType.UnknownError
       );
     }
 
-    let errorType: AsyncStorageError.Type = AsyncStorageError.Type.UnknownError;
+    let errorType: AsyncStorageErrorType = AsyncStorageErrorType.UnknownError;
 
     switch (error.type) {
       case "SqliteException":
-        errorType = AsyncStorageError.Type.SqliteStorageError;
+        errorType = AsyncStorageErrorType.SqliteStorageError;
         break;
       case "OtherException":
-        errorType = AsyncStorageError.Type.OtherStorageError;
+        errorType = AsyncStorageErrorType.OtherStorageError;
         break;
     }
 
@@ -32,49 +56,30 @@ export class AsyncStorageError extends Error {
 
   static jsError(
     error: string,
-    type: AsyncStorageError.Type
+    type: AsyncStorageErrorType
   ): AsyncStorageError {
     return new AsyncStorageError(error, type);
   }
-}
 
-export namespace AsyncStorageError {
-  export enum Type {
-    /**
-     * Related to RN Native module itself, ex. not initialized or null at app boot
-     */
-    NativeModuleError = "NativeModuleError",
-
-    /**
-     * Error thrown from Sqlite itself
-     * https://www.sqlite.org/rescode.html
-     */
-    SqliteStorageError = "SqliteStorageError",
-
-    /**
-     * Other errors related to Native Shared Storage
-     * ex. Storage could not be initialized
-     */
-    OtherStorageError = "OtherStorageError",
-
-    /**
-     * Catch-all case, where we can't really tell what went wrong
-     */
-    UnknownError = "UnknownError",
-  }
+  static Type = AsyncStorageErrorType;
 }
 
 // Native module reject promises with special code
-function isNativeError(e: any): e is PotentialNativeError {
-  return "message" in e && "code" in e && e?.code === "AsyncStorageError";
+function isNativeError(e: unknown): e is PotentialNativeError {
+  if (typeof e !== "object") {
+    return false;
+  }
+
+  const err = e as Record<string, string>;
+  return !!err.message && err?.code === "AsyncStorageError";
 }
 
-function getNativeError(e: any): AsyncStorageNativeError | null {
+function getNativeError(e: unknown): AsyncStorageNativeError | null {
   if (!isNativeError(e)) {
     return null;
   }
 
-  let errorType = e.userInfo ? e.userInfo["type"] : null;
+  const errorType = e.userInfo ? e.userInfo["type"] : null;
 
   if (errorType === "SqliteException") {
     return {
@@ -99,5 +104,5 @@ type AsyncStorageNativeError = {
 type PotentialNativeError = {
   message: string;
   code: "AsyncStorageError";
-  userInfo: Record<string, any> | null;
+  userInfo: Record<string, unknown> | null;
 };
