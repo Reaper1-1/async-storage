e3167f69ef1456db397f3873d0d2a4c85f4ac35e
Krzysztof Borowy <6444719+krizzu@users.noreply.github.com>
2025-09-25 22:20:19 +0200

    fix: export error type

--- packages/async-storage/src/index.tsx
+++ packages/async-storage/src/index.tsx
@@ -3,5 +3,7 @@ import { getLegacyStorage } from "./createAsyncStorage";
 export type { AsyncStorage } from "./AsyncStorage";
 export { createAsyncStorage } from "./createAsyncStorage";
 
+export { AsyncStorageError } from "./AsyncStorageError";
+
 // Legacy storage that is proxy to an old storage data.
 export default getLegacyStorage();
