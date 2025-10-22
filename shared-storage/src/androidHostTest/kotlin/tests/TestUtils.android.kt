9e55055c8af2a6700912233b7445eebd6a509b69
Krzysztof Borowy <6444719+krizzu@users.noreply.github.com>
2025-10-22 17:27:18 +0200

    fix: tests

--- shared-storage/src/androidHostTest/kotlin/tests/TestUtils.android.kt
+++ shared-storage/src/androidHostTest/kotlin/tests/TestUtils.android.kt
@@ -6,7 +6,9 @@ import org.asyncstorage.shared_storage.sharedStorageInMemory
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import kotlin.test.Ignore
 
+@Ignore("Do not run this as test itself")
 @RunWith(RobolectricTestRunner::class) actual open class TestRunner actual constructor()
 
 actual class StorageUtils actual constructor() {
